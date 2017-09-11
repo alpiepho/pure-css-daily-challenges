366f5fc021fd43924c2f436318c31c842b943745
apiepho <al@Al-iMac2.local>
2017-09-11 01:47:23 -0600

    added x/y axis; need mouse over next

--- fcc-dv-codepen/fcc-dv-scatter.js
+++ fcc-dv-codepen/fcc-dv-scatter.js
@@ -1,24 +1,5 @@
-//var url = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json';
 
 var url = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
-
-// Helper function
-/*
-function formatDate(date) {
-  var monthNames = [
-    "January", "February", "March",
-    "April", "May", "June", "July",
-    "August", "September", "October",
-    "November", "December"
-  ];
-
-  var day = date.getDate();
-  var monthIndex = date.getMonth();
-  var year = date.getFullYear();
-
-  return monthNames[monthIndex] + ' ' + day + ', ' + year;
-}
-*/
 
 // MAIN
 d3.json(url, function(jsonData) {
@@ -28,9 +9,7 @@ d3.json(url, function(jsonData) {
   var margin   = { top: 0, right: 0, bottom: 30, left: 20 };
   var height   = 500 - margin.top - margin.bottom;
   var width    = 600 - margin.left - margin.right;
-  var rankings = [];
-  var times    = [];
-  var points   = []; // TODO remove duplication with dollars/dates
+  var points   = []; 
 
   // gather values and dates, so we can properly scale axis
   var maxXData = 0;
@@ -46,8 +25,6 @@ d3.json(url, function(jsonData) {
     minYData = (entry.Place   < minYData ? entry.Place  : minYData);
     points.push(entry);
   }
-
-  
   
   // map y domain to y range
   var yScale = d3.scaleLinear()
@@ -56,14 +33,12 @@ d3.json(url, function(jsonData) {
 
   // map y axis values to range
   var yAxisValues = d3.scaleLinear()
-    .domain([0, maxYData])
+    .domain([maxYData, 1])
     .range([height,0]);
 
-  /*
   // build y axis with 10 ticks over the range
   var yAxisTicks = d3.axisLeft(yAxisValues)
   .ticks(10)
-  */
 
 //  // map x domain to x range and set bar padding
 // var xScale = d3.scaleBand()
@@ -76,16 +51,16 @@ d3.json(url, function(jsonData) {
     .domain([minXData, maxXData])
     .range([0,width]);
 
-  /*
   // map x axis values to range
-  var xAxisValues = d3.scaleTime()
-    .domain([dates[0], dates[(dates.length-1)]])
+  var xAxisValues = d3.scaleLinear()
+    .domain([maxXData/1000, 0])
     .range([0, width])
 
   // build x axis with ticks every 5 years
   var xAxisTicks = d3.axisBottom(xAxisValues)
-    .ticks(d3.timeYear.every(5))
+    .ticks(10)
 
+  /*
   // build tool tip
   var tooltip = d3.select('body')
     .append('div')
@@ -119,7 +94,7 @@ d3.json(url, function(jsonData) {
     .attr('r', '5')
     .attr('fill', function(d) {
       if (d.Doping.length > 0) return "#E4545A";
-      else return "black";
+      else                     return "#555555";
     })
   /*
       .attr('width', function(d) {
@@ -166,13 +141,13 @@ d3.json(url, function(jsonData) {
       */
  
   //Add the SVG Text Element to the svgContainer
-var text = svgContainer.selectAll("text")
+  var text = svgContainer.selectAll("text")
                        .data(points)
                        .enter()
                        .append("text");
 
-//Add SVG Text Element Attributes
-var textLabels = text
+  //Add SVG Text Element Attributes
+  var textLabels = text
   .attr("x", function(d) { return (width - xScale(d.Seconds)) + 10; })
   .attr("y", function(d) { return (yScale(d.Place)) + 4; })
   .text( function (d) { return d.Name })
@@ -181,44 +156,56 @@ var textLabels = text
   .attr("fill", "black");
 
   
-  /*
   // add y guide group using previous y axis
-  var yGuide = d3.select('#bar-chart svg').append('g')
-            .attr('transform', 'translate(20,0)')
+  var yGuide = svgContainer.append('g')
+            .attr('transform', 'translate(-10,0)')
             .call(yAxisTicks);
 
   // add x guide group using previous x axis
-  var xGuide = d3.select('#bar-chart svg').append('g')
-            .attr('transform', 'translate(20,'+ height + ')')
+  var xGuide = svgContainer.append('g')
+            .attr('transform', 'translate(0, '+ (height+10) + ')')
             .call(xAxisTicks);
 
+  /*
+
   // add Title
-  d3.select('#bar-chart svg').append("text")
+  var chartTitle = svgContainer.append("text")
     .attr('x', width / 2 )
     .attr('y', 0)
     .style('text-anchor', 'middle')
     .style('font-weight', 'bold')
     .style('text-decoration', 'underline')
     .text("Gross Domestic Product");
+    */
 
-  // add Y-axis text label
-  d3.select('#bar-chart svg').append("text")
+  // add y-axis text label
+  var yLabel = svgContainer.append("text")
     .attr("transform", "rotate(-90)")
-    .attr("y", 0+margin.left)
-    .attr("x",0 - (height / 2))
+    .attr("y", -90 + margin.left)
+    .attr("x", 0 - (height / 2))
     .attr("dy", "1em")
+    .attr("font-family", "sans-serif")
     .style("text-anchor", "middle")
-    .text("Gross Domestic Product, in $ Billions");  
+    .text("Place Ranking");  
+
+    // add x-axis text label
+  var xLabel = svgContainer.append("text")
+    .attr("y", 40 + height)
+    .attr("x", 0 + (width / 2))
+    .attr("dy", "1em")
+    .attr("font-family", "sans-serif")
+    .style("text-anchor", "middle")
+    .text("Seconds Behind the Leader");  
+
 
   // add Notes below (using separate html div)
   d3.select('#notes')
-    .style('margin-top', '-20px')
-    .style('margin-bottom', '20px')
+    .style('margin-top', '20px')
+    .style('margin-bottom', '50px')
     .style('text-align', 'center')
     .style('font-size', '0.8em')
     .append("text")
-    .text(jsonData.description);
-    */
-});
+    .text("some notes");
 
+});
 
