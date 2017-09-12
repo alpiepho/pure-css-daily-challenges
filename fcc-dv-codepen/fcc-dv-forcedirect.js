d9dbec866aaa81ac4208153bc7bb7b4928f988c7
apiepho <al@Al-iMac2.local>
2017-09-12 07:16:43 -0600

    added my own sprites; cleaned up

--- fcc-dv-codepen/fcc-dv-forcedirect.js
+++ fcc-dv-codepen/fcc-dv-forcedirect.js
@@ -1,58 +1,66 @@
 
 var url = "https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json";
 
-var svg = d3.select("svg");
-var width = +svg.attr("width");
-var height = +svg.attr("height");
-var radius = 100;
+// started with some examples:
+// https://bl.ocks.org/mbostock/4062045  (in d3.js v4)
+// https://bl.ocks.org/mbostock/1129492  (in d3.js v3)
+// NOTE: appearently the API from v3 to v4 changed alot
 
-var color = d3.scaleOrdinal(d3.schemeCategory20);
+// NOTE: also ran across tihs really cool idea.
+//       want to do this with scraped data from LinkedIn
+// https://naustud.io/tech-stack/
+
+
+var svg    = d3.select("svg");                                       // get the svg DOM element
+var width  = +svg.attr("width");
+var height = +svg.attr("height");
+var radius = 20;  // used to keep nodes within a bounds, not a circle
+
+var positionOffsetX = 45;                                            // offset to match svg node
+var positionOffsetY = 105;                                           // offset to match svg node
 
 var simulation = d3.forceSimulation()
     .force("link", d3.forceLink().id(function(d) { return d.id; }))
-    .force("charge", d3.forceManyBody().strength(-10))
-    .force("center", d3.forceCenter(width / 2, height / 2))
-    .force('collision', d3.forceCollide().radius(function(d) {
+    .force("charge", d3.forceManyBody().strength(-10))               // -10 some repel
+    .force("center", d3.forceCenter(width / 2, height / 2))          // center in middle
+    .force('collision', d3.forceCollide().radius(function(d) {       // prevent overlap
       return 20
     }));
 
-d3.json(url, function(graph) {
+d3.json(url, function(graph) {                                       // read from url
 
+  // given data doesn't have id that links refer to, add it
   for (let i = 0; i < graph.nodes.length; i++) {
     var entry = graph.nodes[i];
     entry["id"] = i;
   }
 
+  // add the links
   var link = svg.append("g")
-      .attr("class", "links")
+    .attr("class", "links")
     .selectAll("line")
     .data(graph.links)
     .enter().append("line")
       .attr("stroke", 'grey')
       .attr("stroke-width", '1');
  
-  var node = d3.select(".box").append("div")
+  // add nodes,  nodes are img elements outside svg DOM
+  var node = d3.select(".box")
+    .append("div")
       .attr("class", "nodes")
     .selectAll("img")
     .data(graph.nodes)
     .enter().append("img")
+      // add class names for flags.css sprites
       .attr("class",  function(d) { return ("flag flag-" + d.code); })
       .style("position", "absolute")
+      .style("transform", "scale(2,2)")
       .attr("width", 24)
       .attr("height", 16)
-      //.attr("width", 5)
-      //.attr("height", 5)
-      //.attr("fill", function(d) { return color(d.group); })
-      //.attr("fill", 'blue')
       .call(d3.drag()
           .on("start", dragstarted)
           .on("drag", dragged)
           .on("end", dragended));
-
-  //node.append("title")
-  //    .text(function(d) { return d.country; });
-
-
 
   simulation
       .nodes(graph.nodes)
@@ -66,20 +74,20 @@ d3.json(url, function(graph) {
         .attr("x1", function(d) { return d.source.x; })
         .attr("y1", function(d) { return d.source.y; })
         .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+        .attr("y2", function(d) { return d.target.y; })
+    ;
 
-    //node
-       //.attr("cx", function(d) { return d.x; })
-        //.attr("cy", function(d) { return d.y; });
-       // .attr("x", function(d) { return d.x; })
-       // .attr("y", function(d) { return d.y; });
     node
-      .style("top", function(d) { return  (d.y + 105) +"px";})
-      .style("left", function(d) { return (d.x + 45) +"px";})
-      .attr("x", function(d) { return d.x = Math.max(radius, Math.min(width - radius, d.x)); })
-      .attr("y", function(d) { return d.y = Math.max(radius, Math.min(height - radius, d.y)); });
+      // update svg node position
+      .attr("x",     limitX)
+      .attr("y",     limitY)
+      // update style for img element
+      .style("left", styleX)
+      .style("top",  styleY)
+    ;
   }
 });
+
 
 function dragstarted(d) {
   if (!d3.event.active) simulation.alphaTarget(0.3).restart();
@@ -96,4 +104,20 @@ function dragended(d) {
   if (!d3.event.active) simulation.alphaTarget(0);
   d.fx = null;
   d.fy = null;
+}
+
+function limitX(d) {
+  return d.x = Math.max(radius, Math.min(width - radius, d.x));
+}
+
+function limitY(d) {
+  return d.y = Math.max(radius, Math.min(height - radius, d.y));
+}
+
+function styleX(d) {
+  return (d.x + positionOffsetX) +"px";
+}
+
+function styleY(d) {
+  return  (d.y + positionOffsetY) +"px";
 }
